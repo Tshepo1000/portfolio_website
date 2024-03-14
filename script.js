495f6f27eb4dbe1eae9b429be9aa2afef47a5762
Tshepo1000 <tshepotubatsi@gmail.com>
2024-03-14 15:54:53 +0200

    new updates to my portfolio

--- script.js
+++ script.js
@@ -39,3 +39,39 @@ function erase() {
 }
 
 type(); // Start typewriter effect
+
+//show message box on click
+var s = document.getElementById("messageDivID");
+var sec = document.getElementById("homeSection");
+var hire_me_button = document.getElementById("hire_me");
+var projects_button = document.getElementById("projects");
+
+function showDiv() {
+  s.style.display = (s.style.display == 'none') ? 'block' : 'block';
+  sec.style.background = "linear-gradient(to top, rgba(0, 0, 0, 0.3)50%, rgba(0, 0, 0, 0.3)50%)";
+  sec.style.opacity = "0.5";
+  hire_me_button.style.pointerEvents = "none";
+  projects_button.style.pointerEvents = "none";
+  hire_me_button.style.cursor = "not-allowed";
+  projects_button.style.cursor = "not-allowed";
+}
+
+//hide message box on click
+var h = document.getElementById("closeDiv");
+
+function hideDiv(){
+  s.style.display = (s.style.display == 'block') ? 'none' : 'none';
+  sec.style.background = "white";
+  sec.style.opacity = "1";
+  hire_me_button.style.pointerEvents = "auto";
+  projects_button.style.pointerEvents = "auto";
+  hire_me_button.style.cursor = "pointer";
+  projects_button.style.cursor = "pointer";
+}
+
+//input fields on focus events
+var fname_input = document.getElementById("fname");
+function focusEvents(){
+  fname_input.style.border = "none";
+  fname_input.style.borderBottom = "2px solid black";
+}
